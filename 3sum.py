a75bb1ba8262a893fd636a24c754dc83ba7e5009
yo08 <yosrmanai97@gmail.com>
2023-03-14 23:59:57 +0100

    fixing code by importing List, chnging variables' names for better significance

--- 3sum.py
+++ 3sum.py
@@ -1,24 +1,32 @@
+from typing import List
+
+
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         N = len(nums)
         nums.sort()
         result = []
-        for i in range(N - 2):
-                   if i > 0 and nums[i] == nums[i - 1]:
-                        continue
-                    j = i + 1
-                    k = N - 1
-                    while j < k:
-                        if nums[i] + nums[j] + nums[k] == 0:
-                            result.append([nums[i], nums[j], nums[k]])
-                            while j < k - 1 and nums[k -1] == nums[k]:
-                                k -= 1
-                            while j + 1 < k and nums[j +1] == nums[j]:
-                                j += 1
-                            j += 1
-                            k -= 1
-                        elif nums[i] + nums[j] + nums[k] > 0:
-                            k -= 1
-                        else:
-                            j += 1
+
+        # e1: 1st element in the list
+        # e2: 2nd element in the list
+        # e3: 3rd element in the list
+
+        for e1 in range(N - 2):
+            if e1 > 0 and nums[e1] == nums[e1 - 1]:
+                continue
+            e2 = e1 + 1
+            e3 = N - 1
+            while e2 < e3:
+                if nums[e1] + nums[e2] + nums[e3] == 0:
+                    result.append([nums[i], nums[e2], nums[e3]])
+                    while e2 < e3 - 1 and nums[e3 - 1] == nums[e3]:
+                        e3 -= 1
+                    while e2 + 1 < e3 and nums[e2 + 1] == nums[e2]:
+                        e2 += 1
+                    e2 += 1
+                    e3 -= 1
+                elif nums[e1] + nums[e2] + nums[e3] > 0:
+                    e3 -= 1
+                else:
+                    e2 += 1
         return result
