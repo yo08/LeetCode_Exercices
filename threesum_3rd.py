8b8ceb40664ec6224ebb5ac940ee20a6dd197b4e
yo08 <yosrmanai97@gmail.com>
2023-04-06 22:30:40 +0100

    third solution for 3sum exercice: use set() to avoid duplicates

---
+++ threesum_3rd.py
@@ -0,0 +1,29 @@
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[int]:
+        N = len(nums)
+        nums.sort()
+        results = set()
+
+        for first_index in range(N - 2):
+            if nums[first_index] <= 0:
+                target = -nums[first_index]
+                second_index = first_index + 1
+                third_index = N - 1
+                while (second_index < third_index):
+                    two_sum = nums[second_index] + nums[third_index]
+                    if two_sum < target:
+                        second_index += 1
+                    elif two_sum > target:
+                        third_index -= 1
+                    else:
+                        results.add((nums[first_index], nums[second_index], nums[third_index]))
+                        second_index += 1
+                        third_index -= 1
+        return results
+
+
+if __name__ == "__main__":
+    nums = [-1, 0, 1, 2, -1, -4]
+    test = Solution()
+    results = test.threeSum(nums)
+    print(results)
